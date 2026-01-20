159fdb2837a54587d1ad21092cb57e5dbad64e40
Changhai Man <changhai0109@hotmail.com>
2026-01-20 16:25:41 -0500

    cleanup kernel_launch.cc, separate into start and end and correlate with launchid, collector pending implementation

--- notrace/handlers/kernel_launch.h
+++ notrace/handlers/kernel_launch.h
@@ -1,41 +1,44 @@
 #ifndef __NOTRACE_KERNEL_LAUNCH_H__
 #define __NOTRACE_KERNEL_LAUNCH_H__
 
-#include <cuda_runtime.h>
 #include <stdint.h>
 #include <cstdio>
 #include <vector>
 
+#include "cuda.h"
 #include "cuda_event_handlers.h"
-#include "generated_cuda_meta.h"
 #include "nvbit.h"
 
 namespace notrace {
+namespace kernel_launch {
 
+const uint8_t MESSAGE_TYPE_KERNEL_START = 1;
+const uint8_t MESSAGE_TYPE_KERNEL_END = 2;
+
+#pragma pack(push, 1)
 typedef struct {
-  const char* kernel_name;
-  uint64_t launch_id;
+  uint8_t messageType;
+  uint64_t launchId;
+  uint64_t gpuStartCycles;
 
   CUstream stream;
   CUcontext context;
 
-  uint64_t gpuStartCycles;
-  uint64_t gpuEndCycles;
-
   uint32_t gridX, gridY, gridZ;
   uint32_t blockX, blockY, blockZ;
   uint32_t sharedMemBytes;
-} KernelLaunchInfo;
+} KernelLaunchStartInfo;
+#pragma pack(pop)
 
-// Per-stream anchor used to map CUDA event time -> host time
+#pragma pack(push, 1)
 typedef struct {
-  cudaEvent_t t0;
-  uint64_t host_t0_ns;
-} CudaStreamClock;
+  uint8_t messageType;
+  uint64_t launchId;
+  uint64_t gpuEndCycles;
+} KernelLaunchEndInfo;
+#pragma pack(pop)
 
-void handleKernelLaunch(CUcontext ctx, int device_id, const char* kernel_name,
-                        void* kernel_params, CUresult* pResult);
-
+}  // namespace kernel_launch
 }  // namespace notrace
 
 #endif  // __NOTRACE_KERNEL_LAUNCH_H__
