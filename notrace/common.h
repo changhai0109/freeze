d7b6e984fbeea139d292d269fefdd5170746f62f
Changhai Man <changhai0109@hotmail.com>
2026-01-20 16:24:51 -0500

    add common.h fix nvbit reference problem without nvbit_tool.h

---
+++ notrace/common.h
@@ -0,0 +1,18 @@
+#ifndef __NOTRACE_COMMON_H__
+#define __NOTRACE_COMMON_H__
+
+#include <cuda.h>
+#if CUDA_VERSION >= 12000
+// CUdeviceptr_v1 was removed in CUDA 12, but NVBit needs it.
+// We define it as CUdeviceptr (unsigned long long)
+typedef CUdeviceptr CUdeviceptr_v1;
+typedef CUDA_MEMCPY2D CUDA_MEMCPY2D_v1;
+typedef CUDA_MEMCPY3D CUDA_MEMCPY3D_v1;
+typedef CUDA_ARRAY_DESCRIPTOR CUDA_ARRAY_DESCRIPTOR_v1;
+typedef CUDA_ARRAY3D_DESCRIPTOR CUDA_ARRAY3D_DESCRIPTOR_v1;
+
+#endif
+
+#include "nvbit.h"
+
+#endif
