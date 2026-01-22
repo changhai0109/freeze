c5c0195670c1854b8cb68bf42890da12be85736f
Changhai Man <changhai0109@hotmail.com>
2026-01-22 15:19:48 -0500

    speed up cuda_event producer registry.

--- notrace/cuda_event_handlers.cc
+++ notrace/cuda_event_handlers.cc
@@ -5,24 +5,30 @@
 
 using cuda_event_handler_t = notrace::cuda_event_handler_t;
 
-static std::unordered_map<nvbit_api_cuda_t, cuda_event_handler_t*>
-    cuda_event_handlers;
+// static std::unordered_map<nvbit_api_cuda_t, cuda_event_handler_t*>
+//     cuda_event_handlers;
+static std::vector<cuda_event_handler_t*> cuda_event_handlers(128, nullptr);
 
 void notrace::register_cuda_event_handler(
     nvbit_api_cuda_t cbid, notrace::cuda_event_handler_t* handler) {
+  if (cbid >= cuda_event_handlers.size()) {
+    cuda_event_handlers.resize(cbid * 2, nullptr);
+  }
   cuda_event_handlers[cbid] = handler;
 }
 
 void notrace::unregister_cuda_event_handler(nvbit_api_cuda_t cbid) {
-  cuda_event_handlers.erase(cbid);
+  cuda_event_handlers[cbid] = nullptr;
 }
 
 notrace::cuda_event_handler_t* notrace::get_cuda_event_handler(
     nvbit_api_cuda_t cbid) {
-  auto it = cuda_event_handlers.find(cbid);
-  if (it != cuda_event_handlers.end()) {
-    return it->second;
+  if (cbid < cuda_event_handlers.size() &&
+      cuda_event_handlers[cbid] != nullptr) {
+    return cuda_event_handlers[cbid];
   } else {
+    if constexpr (!notrace::default_handler::ENABLE_DEFAULT_HANDLER)
+      return nullptr;
     notrace::default_handler::setCBID(
         cbid);  // set the cbid for default handler
     return notrace::default_handler::defaultHandlerHookWrapper;
