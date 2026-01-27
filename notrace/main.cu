18d14d332bcbf34ad66fb2fb166a2116361d9f0c
Changhai Man <changhai0109@hotmail.com>
2026-01-27 00:12:26 -0500

    add double check to make sure all message are processed correctly, for debug.

--- notrace/main.cu
+++ notrace/main.cu
@@ -1,34 +1,33 @@
 #include <atomic>
 #include <chrono>
-#include <thread>
 #include <iostream>
+#include <thread>
 
 #include "cuda_event_handlers.h"
-#include "handlers/kernel_launch.h"
-#include "utils/mpsc_queue.h" // Ensure this includes your Queue definition
 #include "nvbit.h"
 #include "nvbit_tool.h"
+#include "utils/mpsc_queue.h"  // Ensure this includes your Queue definition
 
 static std::atomic<bool> g_stop_logging{false};
 static std::thread* g_logger_thread = nullptr;
 
 void logging_worker_loop() {
-    auto& queue = notrace::MPSCMessageQueue::getInstance();
+  auto& queue = notrace::MPSCMessageQueue::getInstance();
 
-    while (!g_stop_logging.load(std::memory_order_relaxed)) {
-        size_t bytes_processed = queue.processUpdates();
+  while (!g_stop_logging.load(std::memory_order_relaxed)) {
+    size_t bytes_processed = queue.processUpdates();
 
-        if (bytes_processed == 0) {
-            std::this_thread::sleep_for(std::chrono::microseconds(10));
-        }
+    if (bytes_processed == 0) {
+      std::this_thread::sleep_for(std::chrono::microseconds(10));
     }
+  }
 
-    queue.processUpdates();
+  queue.processUpdates();
 }
 
 void nvbit_at_init() {
   printf("Tool is being loaded\n");
-  
+
   notrace::register_handlers();
 
   notrace::MPSCMessageQueue::getInstance().registerConsumers();
@@ -38,16 +37,15 @@ void nvbit_at_init() {
 
 void nvbit_at_cuda_event(CUcontext ctx, int is_exit, nvbit_api_cuda_t cbid,
                          const char* name, void* params, CUresult* pStatus) {
+
+  printf("nvbit_at_cuda_event: %s %s\n", is_exit ? "EXIT" : "ENTER", name);
   notrace::cuda_event_handler_t* handler =
       notrace::get_cuda_event_handler(cbid);
-  
+
   // If we have a custom handler (like KernelLaunch), run it.
   if (handler != nullptr) {
     return handler(ctx, is_exit, name, params, pStatus);
   }
-
-  // default action: print event info (optional, might want to disable for performance)
-  // printf("CUDA API %s %s\n", is_exit ? "EXIT" : "ENTER", name);
 }
 
 void nvbit_at_term() {
@@ -61,11 +59,11 @@ void nvbit_at_term() {
 
   // 3. Join the thread (wait for it to finish flushing)
   if (g_logger_thread) {
-      if (g_logger_thread->joinable()) {
-          g_logger_thread->join();
-      }
-      delete g_logger_thread;
-      g_logger_thread = nullptr;
+    if (g_logger_thread->joinable()) {
+      g_logger_thread->join();
+    }
+    delete g_logger_thread;
+    g_logger_thread = nullptr;
   }
 
   printf("Logger stopped. Output flushed.\n");
