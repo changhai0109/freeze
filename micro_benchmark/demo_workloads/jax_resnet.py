a7f2484b2362be0b4a2042d93874d76faf6472ec
Changhai Man <changhai0109@hotmail.com>
2026-02-06 13:38:35 -0500

    fix jax_resnet script

--- micro_benchmark/demo_workloads/jax_resnet.py
+++ micro_benchmark/demo_workloads/jax_resnet.py
@@ -5,20 +5,22 @@ import numpy as np
 
 print(f"JAX Devices: {jax.devices()}")
 
+
 def run_jax_resnet():
     print("--- Running JAX ResNet-50 ---")
     model = FlaxResNetModel.from_pretrained("microsoft/resnet-50")
-    
+
     dummy_input = jnp.ones((1, 3, 224, 224))
-    
+
     @jax.jit
     def inference(inputs):
-        return model(pixel_values=inputs).logits
-    
+        return model(pixel_values=inputs).last_hidden_state
+
     print("Start execution loop...")
     for _ in range(10):
         _ = inference(dummy_input).block_until_ready()
     print("Done.")
+
 
 if __name__ == "__main__":
     run_jax_resnet()
